cf788a11e3d39e94d0e9162a2c1485e03f95d0a1
Rafael Colombo <rcolombodev@gmail.com>
2021-07-30 19:27:15 -0300

    Add: exercises

---
+++ 01 Basics of Web Development/Bloco 4 Introdução - JavaScript/Dia 3 Lógica da Programação/Exercicio2.js
@@ -0,0 +1,10 @@
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+
+let n = 5;
+let linha = "";
+let asteriscos = "*";
+
+for (index = 0; index <= n; index +=1){
+    console.log(linha)
+    linha = linha + asteriscos   
+}

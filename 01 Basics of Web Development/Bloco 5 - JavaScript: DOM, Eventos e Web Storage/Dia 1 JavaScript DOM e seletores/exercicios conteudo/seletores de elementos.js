fa968be0b83e0cc892def870f5b9d25e3bef79df
Rafael Colombo <rcolombodev@gmail.com>
2021-08-09 15:33:09 -0300

    Add exercicios do conteudo

---
+++ 01 Basics of Web Development/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/Dia 1 JavaScript DOM e seletores/exercicios conteudo/seletores de elementos.js
@@ -0,0 +1,4 @@
+document.getElementById('page-title').innerText = 'Avengers';
+document.getElementsByTagName('p')[0].style.color = 'Blue';
+document.getElementById('subtitle').innerText = 'Marvel Studio'
+document.getElementsByTagName('h4')[0].style.color = "Red"

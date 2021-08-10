49d0fd503965099dfebe418e4525ab2fce7905e2
Rafael Colombo <rcolombodev@gmail.com>
2021-08-10 19:38:32 -0300

    Resolucao dos exercicios parte 2

--- 01 Basics of Web Development/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/Dia 2 JavaScript trabalhando com elementos/Exercicios/scripts.js
+++ 01 Basics of Web Development/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/Dia 2 JavaScript trabalhando com elementos/Exercicios/scripts.js
@@ -34,9 +34,9 @@ sectionRight.className = 'right-content';
 main.appendChild(sectionRight);
 
 let image = document.createElement('img');
-image.className = 'small-image'
-image.src = 'https://picsum.photos/200'
-section.appendChild(image)
+image.className = 'small-image';
+image.src = 'https://picsum.photos/200';
+section.appendChild(image);
 
 
 let listNumber = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'];
@@ -45,10 +45,33 @@ sectionLeft.appendChild(ulList)
 for (let index in listNumber) {
   let list = document.createElement('li')
   list.innerText = listNumber[index];
-  ulList.appendChild(list)
+  ulList.appendChild(list);
 }
 
 for (let index = 1; index <=3; index += 1){
-  let h3 = document.createElement('h3')
-  main.appendChild(h3)
+  let h3 = document.createElement('h3');
+  main.appendChild(h3);
 }
+
+// Adicione a classe title na tag h1 criada;
+// Adicione a classe description nas 3 tags h3 criadas;
+// Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
+// Centralize a section criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto da section ;
+// Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
+// Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
+
+h1.className = 'title';
+
+let h3Description = document.getElementsByTagName('h3');
+for (let index = 0; index < 3; index += 1) {
+    h3Description[index].className = 'description';
+}
+
+let sectionRightCenter = document.getElementsByClassName('right-content')[0];
+sectionRightCenter.style.marginRight = 'auto';
+
+let greenCenter = document.getElementsByClassName('center-content')[0];
+greenCenter.parentNode.style.backgroundColor = 'green';
+
+ulList.lastChild.remove();
+ulList.lastChild.remove();

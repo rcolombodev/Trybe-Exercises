3dce901c50861e3c2e7b8a69e978303377695675
Rafael Colombo <rcolombodev@gmail.com>
2021-08-22 09:38:23 -0300

    Adiciona exercicio

--- 01 Basics of Web Development/Bloco 6 HTML e CSS: Forms, Flexbox e Responsivo/Dia 2 Bibliotecas JavaScript e Frameworks CSS/script.js
+++ 01 Basics of Web Development/Bloco 6 HTML e CSS: Forms, Flexbox e Responsivo/Dia 2 Bibliotecas JavaScript e Frameworks CSS/script.js
@@ -1,3 +1,5 @@
+import './node_modules/just-validate/dist/js/just-validate.js'
+
 const select = document.getElementById("estado");
 const states = [
   "Acre",
@@ -41,16 +43,98 @@ const picker = new Pikaday({ field: document.getElementById("datepicker") });
 const field = document.getElementById("datepicker");
 function pickDay() {
   var picker = new Pikaday({
-    onSelect: function (date) {
+    onSelect: function () {
       field.value = picker.toString();
     },
   });
 }
 
-field.addEventListener("keyup", pickDay);
 
 function pagedata() {
   createOptions();
+  new window.JustValidate('.js-form', {
+    rules: {
+      name: {
+        required:true,
+        minLength:3,
+        maxLength:40
+      },
+      cpf: {
+        required:true,
+        maxLength: 11
+      },
+      address: {
+        required:true,
+        maxLength: 200
+      },
+      city: {
+        required:true,
+        maxLength: 28
+      },
+      state: {
+        required:true
+      },
+      radio: {
+        required:true
+      },
+      curriculo: {
+        required:true,
+        maxLength: 1000
+      },
+      cargo: {
+        required:true,
+        maxLength: 40
+      },
+      description: {
+        required:true,
+        maxLength: 500
+      },
+      date: {
+        required:true
+      },
+    },
+    messages: {
+      name: {
+        required: 'O campo de nome é obrigatório.',
+        maxLength: 'O limite é de 40 caracteres.'
+      },
+      cpf: {
+        required: 'O campo de CPF é obrigatório.',
+        maxLength: 'O limite é de 11 caracteres.'
+      },
+      address: {
+        required: 'O campo endereço é obrigatório.',
+        maxLength: 'O limite é de 200 caracteres.'
+      },
+      city: {
+        required: 'O campo cidade é obrigatório.',
+        maxLength: 'O limite é de 28 caracteres.'
+      },
+      state: {
+        required: 'O campo estado é obrigatório.',
+      },
+      radio: {
+        required: 'A escolha de uma opção é obrigatória.',
+      },
+      curriculo: {
+        required: 'Este campo é obrigatório.',
+        maxLength: 'O limite é de 1000 caracteres.'
+      },
+      cargo: {
+        required: 'Este campo é obrigatório.',
+        maxLength: 'O limite é de 40 caracteres.'
+      },
+      description: {
+        required: 'Este campo é obrigatório.',
+        maxLength: 'O limite é de 500 caracteres.'
+      },
+      date: {
+        required: 'Este campo é obrigatório.',
+      }
+    },
+    submitHandler: function (form, values) {
+        console.log(form, values);
+    }});
 }
 
 window.onload = pagedata;

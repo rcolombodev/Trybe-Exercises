6228af1285b025e7083c5d1edcc6f5b684ec738f
Rafael Colombo <rcolombodev@gmail.com>
2021-08-31 21:54:21 -0300

    Adiciona Exercicios

---
+++ 01 Basics of Web Development/Bloco 7  Introdução à JavaScript ES6 e Testes Unitários/Dia 3 Testes unitários em JavaScript/Exercicios/part 2/exercicio2.js
@@ -0,0 +1,19 @@
+//Escreva a função wordLengths para passar nos testes já implementados.
+
+
+const assert = require('assert');
+// escreva a função wordLengths aqui
+const wordLengths = array => {
+  const words = [];
+  for (let index in array){
+    words.push(array[index].length)
+  }
+  return words;
+}
+
+const words = ['sun', 'potato', 'roundabout', 'pizza'];
+const expected = [3, 6, 10, 5];
+
+assert.strictEqual(typeof wordLengths, 'function');
+const output = wordLengths(words);
+assert.deepStrictEqual(output, expected);

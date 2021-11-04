998d56e0d6090e3c52f0db6ebe42414aa7349e61
Rafael Colombo <rcolombodev@gmail.com>
2021-11-04 19:38:31 -0300

    adiciona exercicios

--- 02-Front-End-Development/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercicio-part1/service.js
+++ 02-Front-End-Development/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercicio-part1/service.js
@@ -1,3 +1,9 @@
 const randomNumber = () => Math.floor(Math.random() * 101)
 
-module.exports= {randomNumber} 
+const upperString = (string) => string.toUpperCase();
+
+const firstLetter = (string) => string[0];
+
+const concatString = (string1, string2) => string1.concat(string2);
+
+module.exports= {randomNumber, upperString, firstLetter, concatString} 
--- 02-Front-End-Development/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercicio-part1/service.test.js
+++ 02-Front-End-Development/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercicio-part1/service.test.js
@@ -1,7 +1,10 @@
 const service = require("./service");
-const {it, expect } = require('@jest/globals')
+const {it, expect} = require('@jest/globals')
 
-describe('testando a função randomNumber', () => {
+// jest.mock('./service')
+
+
+describe('Testando a função randomNumber', () => {
   it('deve ser chamada, quantas vezes foi chamada e qual o retorno', () => {
     service.randomNumber = jest.fn().mockReturnValue(10)
     
@@ -11,8 +14,8 @@ describe('testando a função randomNumber', () => {
   });
 });
 
-describe('testando a função randomNumber', () => {
-  it("testando se a função foi chamada, qual seu retorno, quantas vezes foi chamada e com quais parâmetros", () => {
+describe('Testando a função randomNumber', () => {
+  it("Testando se a função foi chamada, qual seu retorno, quantas vezes foi chamada e com quais parâmetros", () => {
     service.randomNumber = jest.fn().mockImplementationOnce((a, b) => a / b)
     
     expect(service.randomNumber(15, 3)).toBe(5);
@@ -22,7 +25,7 @@ describe('testando a função randomNumber', () => {
   });
 });
 
-describe("testando implementações", () => {
+describe("Testando implementações", () => {
   it("mockando função para receber 3 parâmetros e retornar sua multiplicação", () => {
     service.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
 
@@ -42,5 +45,39 @@ describe("testando implementações", () => {
     expect(service.randomNumber).toHaveBeenCalled();
     expect(service.randomNumber).toHaveBeenCalledTimes(1);
     expect(service.randomNumber).toHaveBeenCalledWith(5);
+  });
+});
+
+describe('Testando funções - exercicio 4', () => {
+  it('Testando implementação da função upperString', () => {
+    const upperStringMocked = jest.spyOn(service, 'upperString') 
+    
+    upperStringMocked.mockImplementation(string => string.toLowerCase());
+
+    expect(service.upperString('RAFAEL')).toBe('rafael')
+    expect(service.upperString).toHaveBeenCalled();
+    expect(service.upperString).toHaveBeenCalledTimes(1);
+
+    upperStringMocked.mockRestore();
+
+    expect(service.upperString('rafael')).toBe('RAFAEL')
+  });
+
+  it('Testando implementação da função firstLetter', () => {
+    service.firstLetter = jest.fn().mockImplementation(string => string[string.length - 1]);
+
+    expect(service.firstLetter('RAFAEL')).toBe('L')
+    expect(service.firstLetter).toHaveBeenCalled();
+    expect(service.firstLetter).toHaveBeenCalledTimes(1);
+
+  });
+
+  it('Testando implementação da função concatString', () => {
+    service.concatString = jest.fn().mockImplementation((string1, string2, string3) => string1.concat(string2, string3));
+
+    expect(service.concatString('RAFAEL', ' HENRIQUE', ' COLOMBO')).toBe('RAFAEL HENRIQUE COLOMBO')
+    expect(service.concatString).toHaveBeenCalled();
+    expect(service.concatString).toHaveBeenCalledTimes(1);
+
   });
 });

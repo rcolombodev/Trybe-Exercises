d982d42dad448b8c8f3eeb16ce3edeaf9a5a76e0
rcolombodev <rcolombodev@gmail.com>
2022-04-04 15:08:51 -0300

    Update content Trybe

---
+++ 03-Back-End-Development/bloco-24-node.js_ ORM-e-autenticação/dia-02-ORM-associations/conteudo/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { Address, Employee } = require('./models');
+
+const app = express();
+
+app.get('/employees/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const employee = await Employee.findOne({
+        where: { id },
+        include: [{ model: Address, as: 'addresses' }],
+      });
+
+    if (!employee)
+      return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+    return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
+
+module.exports = app;
